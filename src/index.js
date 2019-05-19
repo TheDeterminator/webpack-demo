cc293a71c8c27a703c8d7b93087e33a269e21f89
Alec <alec.jordan.cs@gmail.com>
2019-05-18 21:15:08 -0400

    prepare output management lesson

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Prof from './prof-photo.jpg';
-import Data from './data.xml';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Prof;
-
-  element.appendChild(myIcon);
-
-	console.log(Data)
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,40 +5,5 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-	{
-	test: /\.css$/,
-		use: [
-		'style-loader',
-		'css-loader'
-		]
-	},
-	{
-	  test: /\.(png|svg|jpg|fig)$/,
-		use: [
-			'file-loader'
-		]
-	},
-	    {
-	test: /\.(woff|woff2|eot|ttf|otf)$/,
-	use: [
-		'file-loader'
-	]
-	    },
-			{
-				test: /\.xml$/,
-				use: [
-					'xml-loader'
-				]
-			},
-			{
-				test: /\.(csv|tsv)$/,
-				use: [
-					'csv-loader'
-				]
-			}
-      ]
-    }
+  }
 };
