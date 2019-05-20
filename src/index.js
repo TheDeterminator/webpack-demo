acd8b0f79958bede1be357996768669676075362
Alec <alec.jordan.cs@gmail.com>
2019-05-19 23:27:23 -0400

    adds prettier formatting

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import _ from 'lodash';
 //import Print from './print';
 
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
- //  element.onclick = Print.bind(null, 'Hello webpack!');
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  //  element.onclick = Print.bind(null, 'Hello webpack!');
 
-    return element;
-  }
+  return element;
+}
 
-  document.body.appendChild(component());
+document.body.appendChild(component());
